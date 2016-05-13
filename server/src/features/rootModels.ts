51267f74180905b6a72133361b0919d8f6cd6993
renelimadev <renelima.dev@gmail.com>
2016-05-12 18:40:43 -0700

    rootModels.ts updated

--- server/src/features/rootModels.ts
+++
@@ -1,9 +0,0 @@
-import { Todos } from './todos/model';
-import { UsersModel } from './users/model';
-/* CODE-GENERATOR - ROOTMODELS IMPORT */
-
-export default {
-  Todos,
-  UsersModel,
-  /* CODE-GENERATOR - ROOTMODELS DEFAULT */
-};
